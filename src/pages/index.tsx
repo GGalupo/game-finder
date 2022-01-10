cd6be3fec664638f904c443ce4a62e5d851c2824
Gustavo Galupo <ggalupo2@hotmail.com>
2022-01-10 18:23:36 -0300

    Faz a requisição da lista de jogos na API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import axios from "axios";
+
+interface Game {
+  id: number;
+  title: string;
+  imgUrl: string;
+  description: string;
+  gameUrl: string;
+  releaseDate: string;
+}
+
 const Home: NextPage = () => {
+  const [gamesList, setGamesList] = useState<Game[]>([]);
+
+  useEffect(() => {
+    const getGamesList = async () => {
+      try {
+        const response = await axios("https://mmo-games.p.rapidapi.com/games", {
+          headers: {
+            "x-rapidapi-host": "mmo-games.p.rapidapi.com",
+            "x-rapidapi-key": process.env.RAPIDAPI_KEY!,
+          },
+        });
+
+        setGamesList(response.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    getGamesList();
+  }, []);
+
+  console.log(gamesList);
   return (
     <>
       <Head>
