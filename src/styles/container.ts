d11ded62cf51512a7a12bde46f11cbae1ba944ac
Gustavo Galupo <ggalupo2@hotmail.com>
2022-01-12 03:58:53 -0300

    Estilos adicionais container

--- src/styles/container.ts
+++ src/styles/container.ts
@@ -4,4 +4,9 @@ export const StyledContainer = styled.main`
   max-width: 1120px;
   margin: 2rem auto 5rem;
   padding: 0 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
 `;
